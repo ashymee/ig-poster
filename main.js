db36b164b66618a35f7aceb18066e2e36ea097af
ashymee <38484500+ashymee@users.noreply.github.com>
2021-07-16 13:09:49 +0000

    Deploying to gh-pages from @ ashymee/ig-poster@65c89d788a5633565a23cc6bbcc12f0dff19b213 🚀

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
 var app = express_1.default();
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.get('/', function (req, res) {
     res.send('welcome..');
 });
