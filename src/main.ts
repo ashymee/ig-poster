ab754c6abeb53f7b9fc731015feff2a57746487d
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-16 19:46:20 +0700

    update res

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Halo halo hai')
+  res.send('welcome..')
 })
 
 const port = process.env.PORT || 9999
