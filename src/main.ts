65c89d788a5633565a23cc6bbcc12f0dff19b213
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-16 20:09:13 +0700

    add tools & serve static files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('welcome..')
 })
